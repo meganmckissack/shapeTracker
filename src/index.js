29216293b9a7ffd0c8585eb920d1333afa5baf6b
Megan McKissack <meganmckissack@gmail.com>
2022-05-08 15:56:02 -0700

    updated test driven dev requirements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
 
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
@@ -5,7 +10,7 @@ $(document).ready(function() {
     const length1 = $('#length1').val();
     const length2 = $('#length2').val();
     const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
+    const triangle = new Triangle(length1, length2, length3);
     const response = triangle.checkType();
     $('#response').append("<p>" + response + "</p>");
   });
