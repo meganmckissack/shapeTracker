4623eae2d44010325d6afd3e7184f1906a513de1
Megan McKissack <meganmckissack@gmail.com>
2022-05-08 16:43:53 -0700

    updated triangle tests

--- _tests_/triangle.test.js
+++ _tests_/triangle.test.js
@@ -23,4 +23,16 @@ describe('Triangle', () => {
     const isocTriangle = new Triangle(5,5,7);
     expect(isocTriangle.checkType()).toEqual("isosceles triangle");
   });
+
+  Triangle.prototype.checkType = function() {
+    if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+      return "not a triangle";
+    } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+      return "scalene triangle";
+    } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+      return "equilateral triangle";
+    } else {
+      return "isosceles triangle";
+    }
+  };
 });
